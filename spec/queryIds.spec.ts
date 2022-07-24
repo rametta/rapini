ca80430d9d60e5f11f20381c2a472bbbfc48d42f
Jason Rametta <jason.rametta@unity3d.com>
2022-07-24 17:38:03 -0400

    Add test file for QueryIds generation

---
+++ spec/queryIds.spec.ts
@@ -0,0 +1,100 @@
+import type { OpenAPIV3 } from "openapi-types";
+import { makeQueryIds } from "../src/queryIds";
+import { compile } from "./test.utils";
+
+const expected = `function nullIfUndefined<T>(value: T): T | null {
+    return typeof value === "undefined" ? null : value;
+}
+function makeQueryIds() {
+    return {
+        getPets: () => ["getPets"] as const,
+        getPet: (petId: string) => ["getPet", petId] as const
+    } as const;
+}
+`;
+
+describe("makeQueryIds", () => {
+  it("generates queryIds for every path", () => {
+    const doc: OpenAPIV3.Document = {
+      openapi: "3.0.0",
+      info: {
+        title: "Test",
+        version: "1.0.0",
+      },
+      paths: {
+        "/pets": {
+          get: {
+            operationId: "getPets",
+            responses: {
+              default: {
+                description: "anything",
+                content: {
+                  "application/json": {
+                    schema: {
+                      $ref: "",
+                    },
+                  },
+                },
+              },
+            },
+          },
+          post: {
+            operationId: "createPet",
+            requestBody: {
+              description: "anything",
+              content: {
+                "application/json": {
+                  schema: {
+                    $ref: "",
+                  },
+                },
+              },
+            },
+            responses: {
+              default: {
+                description: "anything",
+                content: {
+                  "application/json": {
+                    schema: {
+                      $ref: "",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        "/pets/{petId}": {
+          get: {
+            operationId: "getPet",
+            parameters: [
+              {
+                name: "petId",
+                in: "path",
+                required: true,
+                schema: {
+                  type: "string",
+                },
+              },
+            ],
+            responses: {
+              default: {
+                description: "anything",
+                content: {
+                  "application/json": {
+                    schema: {
+                      $ref: "",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const str = compile(makeQueryIds(doc.paths));
+    expect(str).toBe(expected);
+  });
+});
