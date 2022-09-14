29fc2772c930220ffd9a2aee2b19577847b0d3c6
Jason Rametta <jason.rametta@unity3d.com>
2022-09-14 06:24:53 +0300

    Update unit tests for import statements

--- spec/import.spec.ts
+++ spec/import.spec.ts
@@ -1,13 +1,23 @@
+import { CLIOptions } from "../src/cli";
 import { makeImports } from "../src/imports";
 import { compile } from "./test.utils";
 
-const expected = `import type { AxiosInstance, AxiosRequestConfig } from "axios";
+const expectedV3 = `import type { AxiosInstance, AxiosRequestConfig } from "axios";
 import { useQuery, useMutation, useQueryClient, type QueryClient, type UseMutationOptions, type UseQueryOptions, type MutationFunction, type UseMutationResult, type UseQueryResult } from "react-query";
 `;
 
+const expectedV4 = `import type { AxiosInstance, AxiosRequestConfig } from "axios";
+import { useQuery, useMutation, useQueryClient, type QueryClient, type UseMutationOptions, type UseQueryOptions, type MutationFunction, type UseMutationResult, type UseQueryResult } from "@tanstack/react-query";
+`;
+
 describe("makeImports", () => {
-  it("generates the correct import statements", () => {
-    const str = compile(makeImports());
-    expect(str).toBe(expected);
+  it("generates the correct import statements for v3", () => {
+    const str = compile(makeImports({ reactQueryV4: false } as CLIOptions));
+    expect(str).toBe(expectedV3);
+  });
+
+  it("generates the correct import statements for v4", () => {
+    const str = compile(makeImports({ reactQueryV4: true } as CLIOptions));
+    expect(str).toBe(expectedV4);
   });
 });
