bd33f668a87b50d94c62a6c777860ddb9be1ee6d
Jason Rametta <jason.rametta@unity3d.com>
2022-10-12 10:24:48 -0400

    Revamp CLI options and bump to 2.0.0

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { generate as generateReactQuery } from "./react-query/generator";
 import { generate as generateSWR } from "./swr/generator";
-import { Argument, Command } from "commander";
+import { Argument, Command, Option } from "commander";
 
 export type CLIOptions = {
   name: string;
@@ -13,51 +13,61 @@ export type CLIOptions = {
   reactQueryV4: boolean;
 };
 
-type CLIArgLibrary = "swr" | "react-query";
-
 const program = new Command();
 
-program
-  .version("1.11.0")
-  .description("Generate a package based on OpenAPI")
-  .requiredOption("-p, --path <path>", "Path to OpenAPI file")
-  .option(
+const sharedOptions = [
+  new Option("-p, --path <path>", "Path to OpenAPI file"),
+  new Option(
     "-n, --name [name]",
-    "Name to use for the generated package",
-    "rapini-generated-package"
-  )
-  .option(
+    "Name to use for the generated package"
+  ).default("rapini-generated-package"),
+  new Option(
     "-pv, --package-version [version]",
-    "Semver version to use for the generated package",
-    "1.0.0"
-  )
-  .option(
+    "Semver version to use for the generated package"
+  ).default("1.0.0"),
+  new Option(
     "-o, --output-dir [directory]",
-    "Directory to output the generated package",
-    "rapini-generated-package"
-  )
-  .option("-b, --base-url [url]", "Prefix every request with this url")
-  .option(
+    "Directory to output the generated package"
+  ).default("rapini-generated-package"),
+  new Option("-b, --base-url [url]", "Prefix every request with this url"),
+  new Option(
     "-r, --replacer [oldString] [newString...]",
     "Replace part(s) of any route's path with simple string replacements. Ex: `-r /api/v1 /api/v2` would replace the v1 with v2 in every route"
-  )
-  .option(
-    "-rq-v4, --react-query-v4",
-    "Use React Query V4 aka '@tanstack/react-query'",
-    false
-  );
+  ),
+];
 
-program.addArgument(
-  new Argument("[library]", "Library for generating code")
-    .choices(["swr", "react-query"])
-    .default("react-query")
-);
+let rqCommand = new Command("react-query")
+  .description("Generate a Package for TanStack Query V4 or React Query V3")
+  .addArgument(
+    new Argument("[version]")
+      .choices(["v3", "v4"])
+      .default("v3", "React Query V3 is the default version")
+  )
+  .action((version, options) => {
+    console.log(
+      `Generating React Query ${version} package using OpenApi file ${options.path}`
+    );
+    options.reactQueryV4 = version === "v4";
+    generateReactQuery(options);
+  });
+
+let swrComamnd = new Command("swr")
+  .description("Generate a Package for SWR (stale-while-revalidate)")
+  .action((options) => {
+    console.log(`Generating SWR package using OpenApi file ${options.path}`);
+    generateSWR(options);
+  });
+
+sharedOptions.forEach((option) => {
+  rqCommand = rqCommand.addOption(option);
+  swrComamnd = swrComamnd.addOption(option);
+});
+
+program
+  .name("rapini")
+  .description("Generate a package based on OpenAPI")
+  .version("2.0.0")
+  .addCommand(rqCommand)
+  .addCommand(swrComamnd);
 
 program.parse();
-
-const argLibrary = (program.args[0] || "react-query") as CLIArgLibrary;
-const options = program.opts<CLIOptions>();
-
-console.log(`Generating package using OpenApi file ${options.path}`);
-if (argLibrary === "react-query") generateReactQuery(options);
-else if (argLibrary === "swr") generateSWR(options);
